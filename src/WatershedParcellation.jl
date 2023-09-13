b561bd97b064e3f9a176872dc5e18ddfa405824c
Michael Myers <michael.myers@wustl.edu>
2023-09-13 07:42:54 -0500

    Organized code into gen and eval folders

--- src/WatershedParcellation.jl
+++ src/WatershedParcellation.jl
@@ -5,8 +5,8 @@ const nverts = 59412
 const VertexList = Vector{Vector{UInt16}}
 const assets_dir = joinpath(@__DIR__, "../data/32k_tools/")
 
-include("minima.jl")
-include("watershed.jl")
+include("generation/minima.jl")
+include("generation/watershed.jl")
 
 end
 
