65dac1a7eeba3b219b40bb8e911dc8d88b44d381
Michael Myers <120071679+myersm0@users.noreply.github.com>
2023-09-12 20:29:23 -0500

    Update demo.jl

--- src/demo.jl
+++ src/demo.jl
@@ -2,7 +2,7 @@
 grads = load_gradients(filename)
 neigh = load_neighbors()
 adjmat = make_adjmat(neigh)
-minima = make_minima_metrics(grads, adjmat)
+minima = find_minima(grads, adjmat)
 edgemap = run_watershed(grads, minima, neigh)
 
 
