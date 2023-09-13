1f45ab47dfe915c9444c2441b9da62705a576d5d
Michael Myers <michael.myers@wustl.edu>
2023-09-12 23:22:22 -0500

    Accidentally wrote data1 instead of data

--- src/WatershedParcellation.jl
+++ src/WatershedParcellation.jl
@@ -3,7 +3,7 @@ module WatershedParcellation
 
 const nverts = 59412
 const VertexList = Vector{Vector{UInt16}}
-const assets_dir = joinpath(@__DIR__, "../data1/32k_tools/")
+const assets_dir = joinpath(@__DIR__, "../data/32k_tools/")
 
 include("minima.jl")
 include("watershed.jl")
