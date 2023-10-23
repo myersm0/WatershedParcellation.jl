ca109a6d56d4955449c71323c2dc00259c1fbf38
Michael Myers <michael.myers@wustl.edu>
2023-10-22 23:24:29 -0500

    Fixed minima and made it faster

--- src/minima.jl
+++ src/minima.jl
@@ -2,36 +2,41 @@
 export find_minima
 
 function find_minima(metric::AbstractMatrix, Aᵖ::AbstractMatrix, v::Int)
-	neighbors = setdiff(findall(Aᵖ[v, :] .!= 0), v)
+	neighbors = setdiff(findall(Aᵖ[:, v] .!= 0), v)
 	a = repeat(metric[v, :]', outer = [length(neighbors), 1])
 	b = @view metric[neighbors, :]
 	return all(b - a .> 0; dims = 1)
 end
 
-"""
-    find_minima(metric, A; power = 3)
-
-Given a matrix `metric`, such as a gradient matrix, find local minima within a
-window of size `radius` steps, guided by the topology given in adjaceny matrix `A`
-"""
-function find_minima(metric::AbstractMatrix, A::SparseMatrixCSC; power::Int = 3)
+function find_minima(metric::AbstractMatrix, A::SparseMatrixCSC; radius::Int = 3)
 	nverts = size(metric, 1)
 	minima = BitMatrix(undef, nverts, nverts)
+	Aᵖ = A ^ radius
 	Threads.@threads for v in 1:nverts
-		minima[v, :] = find_minima(metric, A^power, v)
+		minima[:, v] = find_minima(metric, Aᵖ, v)
 	end
-	return minima
+	return minima'
 end
 
-function find_minima(metric::AbstractMatrix, hem::Hemisphere; power::Int = 3)
-	haskey(hem, :A) || error("Hemisphere must contain adjacency matrix :A")
+"""
+    find_minima(metric, surface; radius = 3)
+
+Given a matrix `metric`, such as a gradient matrix, find local minima within a
+window of size `radius` steps. Argument `surface <: SurfaceSpace` must contain 
+symbol `:A` (an adjacency matrix)
+"""
+function find_minima(metric::AbstractMatrix, surface::SurfaceSpace; radius::Int = 3)
+	haskey(surface, :A) || error("surface must contain adjacency matrix :A")
 	m, n = size(metric)
 	allequal([m, n]) || error("`metric` must be a square Matrix")
-	if m == size(hem, Inclusive())
-		return find_minima(metric, hem[:A]; power = power)
-	elseif m == size(hem, Exclusive())
-		return find_minima(metric, hem[:A, Exclusive()]; power = power)
+	if m == size(surface, Inclusive())
+		A = surface[:A]
+	elseif m == size(surface, Exclusive())
+		A = surface[:A, Exclusive()]
+	else
+		error(DimensionMismatch)
 	end
+	return find_minima(metric, A; radius = radius)
 end
 
 
