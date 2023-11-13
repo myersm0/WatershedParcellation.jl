d8a91e47255adaa03c0c48a5ece136f113cb4c20
Michael Myers <120071679+myersm0@users.noreply.github.com>
2023-11-13 08:16:49 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using WatershedParcellation
 using Test
 
+# I'm working on figuring out how to incorporate tests. So far it's been tested only informally.
+# One difficulty is that testing requires several large objects (up to 14 GB each) as inputs
+# and I don't know where I can store these. My solution is probably going to be to test it on
+# a reduced-dimension set of inputs (4k instead of 32k)
+
 @testset "WatershedParcellation.jl" begin
     # Write your tests here.
 end
